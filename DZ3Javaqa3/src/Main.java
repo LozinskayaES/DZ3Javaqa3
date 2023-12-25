542ad6157748a50c109e8c649c0577bd0a6d8812
Evgeniya <151749420+LozinskayaES@users.noreply.github.com>
2023-12-25 16:28:42 +0300

    Update Main.java

--- DZ3Javaqa3/src/Main.java
+++ DZ3Javaqa3/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double S = 1_000_000;
-        double P = 9.99;
-        double N = 2;
+        double amountOfCredit = 1_000_000;
+        double annualInterestRate = 9.99;
+        double numberOfMonths = 2;
 
-        double Se = service.calculate(S, P, N);
+        double annuityPayments = service.calculate(amountOfCredit, annualInterestRate, numberOfMonths);
 
-        System.out.println("Ваш ежемесячный платёж: " + Se);
+        System.out.println("Ваш ежемесячный платёж: " + annuityPayments);
     }
 }
